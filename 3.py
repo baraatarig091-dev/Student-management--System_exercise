3302139cbe4e9b0b248a2dc99f6faf62ce727873
baraatarig091-dev <baraatarig091@gmail.com>
2026-07-06 12:19:30 +0200

    Create mine.3py

---
+++ 3.py
@@ -0,0 +1,10 @@
+#inputprint(result)
+#student_gba =75
+student_gba = 75
+#pass_degree=50
+pass_degree=50
+##process
+#result= student_gba >=pass_degree
+result= student_gba >=pass_degree
+##output
+print("result")
